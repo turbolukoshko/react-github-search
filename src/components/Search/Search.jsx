55a81732c2740e1aa2b49133ae7c814ca7db8496
turbolukoshko <turbo.lukoshko@gmail.com>
2021-03-30 16:28:12 +0300

    refactoring seach component, added validation

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,19 +1,33 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AlertContext } from '../../context/alert/alertContext';
 
 const Search = () => {
 
+  const [value, setValue] = useState('');
   const {show} = useContext(AlertContext);
 
   const onSubmit = event => {
-    if(event.key === 'Enter') {
-      show('This is alert');
+    if(event.key !== 'Enter') {
+      return;
+    }
+
+    if(value.trim()) {
+      console.log(`Make request with: ${value}`);
+    }else {
+      show('Enter user data');
     }
   }
 
   return(
     <div className="form-group">
-      <input type="text" className="form-control" placeholder="Enter username" onKeyPress={onSubmit}/>
+      <input 
+        type="text" 
+        onKeyPress={onSubmit}
+        value={value}
+        onChange={e => setValue(e.target.value)}
+        className="form-control" 
+        placeholder="Enter username" 
+      />
     </div>
   );
 }
