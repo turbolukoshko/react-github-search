541d141432c6276cef31ccd47a0f76b89a1cc0c8
turbolukoshko <turbo.lukoshko@gmail.com>
2021-03-30 16:21:46 +0300

    refactoring alert context, added reducer, types

--- src/components/Alert/Alert.jsx
+++ src/components/Alert/Alert.jsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AlertContext } from "../../context/alert/alertContext";
 
-const Alert = ({alert}) => {
+const Alert = () => {
+  const {alert, hide} = useContext(AlertContext);
+
+  if(!alert) return null;
 
   const {type, text} = alert;
   return (
@@ -13,6 +17,7 @@ const Alert = ({alert}) => {
         type="button"
         className="close"
         aria-label="Close"
+        onClick={hide}
       >
         <span aria-hidden="true">&times;</span>
       </button>
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AlertContext } from '../../context/alert/alertContext';
 
 const Search = () => {
+
+  const {show} = useContext(AlertContext);
+
+  const onSubmit = event => {
+    if(event.key === 'Enter') {
+      show('This is alert');
+    }
+  }
+
   return(
     <div className="form-group">
-      <input type="text" className="form-control" placeholder="Enter username" />
+      <input type="text" className="form-control" placeholder="Enter username" onKeyPress={onSubmit}/>
     </div>
   );
 }
